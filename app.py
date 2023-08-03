4bea72559075f70ab2f6ed6a0000caf4319273b4
Adam Scanga <59103444+sandmanscanga@users.noreply.github.com>
2023-08-03 03:15:51 -0400

    Fixed formatting on the app.py script.

--- app.py
+++ app.py
@@ -8,42 +8,48 @@ app = Flask(__name__)
 def live_code_challenges():
     """The live-code-challenges page view."""
 
-    return render_template("live-code-challenges.html", title="Live Code Challenges")
+    return render_template("live-code-challenges.html",
+                           title="Live Code Challenges")
 
 
 @app.route("/functions")
 def functions():
     """The functions page view."""
 
-    return render_template("functions.html", title="Functions")
+    return render_template("functions.html",
+                           title="Functions")
 
 
 @app.route("/data-types")
 def data_types():
     """The data-types page view."""
 
-    return render_template("data-types.html", title="Data Types")
+    return render_template("data-types.html",
+                           title="Data Types")
 
 
 @app.route("/data-structures")
 def data_structures():
     """The data-structures page view."""
 
-    return render_template("data-structures.html", title="Data Structures")
+    return render_template("data-structures.html",
+                           title="Data Structures")
 
 
 @app.route("/control-flow-logic")
 def control_flow_logic():
     """The control-flow-logic page view."""
 
-    return render_template("control-flow-logic.html", title="Control Flow Logic")
+    return render_template("control-flow-logic.html",
+                           title="Control Flow Logic")
 
 
 @app.route("/builtin-functions")
 def builtin_functions():
     """The builtin-functions page view."""
 
-    return render_template("builtin-functions.html", title="Builtin Functions")
+    return render_template("builtin-functions.html",
+                           title="Builtin Functions")
 
 
 @app.route("/")
@@ -51,7 +57,8 @@ def builtin_functions():
 def assessment():
     """The assessment page view."""
 
-    return render_template("assessment.html", title="Python Assessment")
+    return render_template("assessment.html",
+                           title="Python Assessment")
 
 
 if __name__ == "__main__":
